6a6eb73593662bc7df9e25b43d3dbe6f646d1b44
Randy Leclerc <randy.leclerc@laposte.net>
2019-05-22 12:24:27 +0200

    Add DB

--- BLL/UserService/Domain/ApplicationUserDomain.cs
+++ BLL/UserService/Domain/ApplicationUserDomain.cs
@@ -6,6 +6,7 @@ namespace BLL.UserService.Domain
 {
     public class ApplicationUserDomain
     {
+        //Something
         public string Id { get; set; }
         public string FirstName { get; set; }
     }
